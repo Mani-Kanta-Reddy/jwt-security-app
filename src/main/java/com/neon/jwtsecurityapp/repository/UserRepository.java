901e29bc2c3ec34c2128be54dbc7efb7d113d384
Ummadi, Mani Kanta Reddy <ManiKantaReddy_Ummadi@comcast.com>
2023-06-10 12:05:30 +0530

    Adding User Repository & Entity for Identity management

---
+++ src/main/java/com/neon/jwtsecurityapp/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.neon.jwtsecurityapp.repository;
+
+import com.neon.jwtsecurityapp.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>
+{
+    Optional<User> findByEmail(String email);
+}
